5c4cf13074a128e905adcfad9eb1cc9705b71cf0
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-03-03 18:59:12 +0100

    Fix api response

--- utility/faucet.js
+++ utility/faucet.js
@@ -1,24 +1,20 @@
 const axios = require("axios");
 
 const callFaucet = (address, agent, callback) => {
-  try {
-    axios({
-      method: "POST",
-      url: process.env.FAUCET_SERVER_URL,
-      data: {
-        address,
-        agent
-      }
+  axios({
+    method: "POST",
+    url: process.env.FAUCET_SERVER_URL,
+    data: {
+      address,
+      agent
+    }
+  })
+    .then(result => {
+      callback(result.data);
     })
-      .then(result => {
-        console.log(result);
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  } catch (err) {
-    console.log(err);
-  }
+    .catch(error => {
+      callback(error.response.data.error);
+    });
 };
 
 module.exports = {
